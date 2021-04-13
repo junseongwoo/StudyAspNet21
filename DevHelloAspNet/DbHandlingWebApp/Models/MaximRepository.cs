66281c5f3fcd7e7cb70f2cdc852bbabe7bb924a4
junseongwoo <wnstjd4852@gmail.com>
2021-04-13 17:01:50 +0900

    0413 커밋

---
+++ DevHelloAspNet/DbHandlingWebApp/Models/MaximRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DbHandlingWebApp.Models
+{
+    public class MaximRepository
+    {
+        private IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString);
+
+        // 입력 
+        public Maxims AddMaxims(Maxims maxim)
+        {
+            string query = @"INSERT INTO Maxims (Name, Contents) Values (@Name, @Contents)
+                             SELECT CASE(SCOPE_IDENTITY() AS INT);";
+
+            var id = db.Query(query, maxim).Single();
+            maxim.Id = id;
+            return maxim;
+        }
+    }
+}
---
+++ DevHelloAspNet/DbHandlingWebApp/Models/Memos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DbHandlingWebApp.Models
+{
+    public class Memos
+    {
+        public int Num { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime PostDate { get; set; }
+
+        public string PostIP { get; set; }
+    }
+}
