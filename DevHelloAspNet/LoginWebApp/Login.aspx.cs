157a71efe6403d1473338051d3e79c4f76d41b5b
junseongwoo <wnstjd4852@gmail.com>
2021-04-14 15:18:14 +0900

    0414 커밋

---
+++ DevHelloAspNet/LoginWebApp/Login.aspx.cs
@@ -0,0 +1,46 @@
+﻿using LoginWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginWebApp
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnRegister_Click(object sender, EventArgs e)
+        {
+            var repo = new Repository();
+            
+            if(repo.IsCorrectUser(TxtUserID.Text, TxtPassword.Text))
+            {
+                // 성공
+                // 인증부여
+                if (!string.IsNullOrEmpty(Request["ReturnUrl"]))
+                {
+                    FormsAuthentication.RedirectFromLoginPage(TxtUserID.Text, false);
+                }
+                else
+                {
+                    FormsAuthentication.SetAuthCookie(TxtUserID.Text, false);
+                    Response.Redirect("~/Welcome.aspx");
+                     
+                }
+            }
+            else
+            {
+                // 실패
+                Response.Write("<script>alsert('잘못된 사용자입니다!');</script>");
+                Response.End();
+            }
+        }
+    }
+}
