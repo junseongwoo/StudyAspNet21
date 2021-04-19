7aa918026fb68b5e2c1d2bbdbf15a0f2645553af
junseongwoo <wnstjd4852@gmail.com>
2021-04-19 17:07:23 +0900

    0419 커밋

---
+++ MvcSolutions/DBWebApp/Models/Room.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBWebApp.Models
+{
+    public class Room
+    {
+        [Key]
+        public int RoomId { get; set; }
+
+        [Required] // 널 값 허용 하지 않음
+        public string Name { get; set; }
+
+        public virtual ICollection<Speaker> Speakers { get; set; }
+    }
+
+    public class Speaker
+    {
+        [Key]
+        public int SpeakerId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int RoomId { get; set; }
+
+        public virtual Room Room { get; set; }
+    }
+}
---
+++ MvcSolutions/TodoListWepApp/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListWepApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        // 생성자 AppDbContext 가 없으면 DB 연결 안됌 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
