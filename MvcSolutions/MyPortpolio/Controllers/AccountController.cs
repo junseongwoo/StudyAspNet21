b7e565aa98f10f7cdfd0be939ec0a8d52ea0ba31
junseongwoo <wnstjd4852@gmail.com>
2021-04-21 12:47:54 +0900

    0421 오류 나지만 커밋

---
+++ MvcSolutions/MyPortpolio/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyPortpolio.Data;
+using MyPortpolio.Models;
+
+namespace MyPortpolio.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            var account = new Account();
+            return View(account);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Login([Bind("Email,Password")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = CheckAccount(account.Email, account.Password);
+                if (result==null) // 계정이 없음
+                { // 계정이 없음 화면을 Home/Index 이동
+
+                }
+                else
+                { // 로그인
+
+                }
+            }
+        }
+
+        private Account CheckAccount(string email, string password)
+        {
+
+        }
+    }
+}
---
+++ MvcSolutions/MyPortpolio/Controllers/ContactsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyPortpolio.Data;
+using MyPortpolio.Models;
+
+namespace MyPortpolio.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contacts
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([Bind()] Contact contact)
+        {
+
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Email,Contents,RegDate")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contact);
+        }
+
+
+    }
+}
